160720752d8f8ccf22f138a7bdd039d52f51bfd2
ak29 <ak229@buffalo.edu>
2015-12-06 02:52:18 -0500

    Completed Language Element; fixed Location

--- js/search.js
+++ js/search.js
@@ -12,3 +12,14 @@ $('#tags').tagsInput({
    // 'maxChars' : 0, // if not provided there is no limit
    'placeholderColor' : '#666666'
 });
+
+function yesnoCheck() {
+    if (document.getElementById('countryCheck').checked) {
+        document.getElementById('isCountry').style.display = 'block';
+         document.getElementById('isCoord').style.display = 'none';
+    }
+    else {
+      document.getElementById('isCountry').style.display = 'none';
+      document.getElementById('isCoord').style.display = 'block';
+   }
+}
